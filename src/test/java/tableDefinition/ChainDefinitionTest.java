0b325bd7987dd9c2af808e841ebcfd4a7077de18
Alexey Sandler <as371x@intl.att.com>
2018-04-24 12:13:52 +0300

    Unit tests are working.

--- src/test/java/tableDefinition/ChainDefinitionTest.java
+++ src/test/java/tableDefinition/ChainDefinitionTest.java
@@ -1,53 +1,60 @@
-//package tableDefinition;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import java.sql.Connection;
-//import java.sql.SQLException;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class ChainDefinitionTest {
-//    ChainDefinition emptyDefinition = new ChainDefinition();
-//    ConnectionToDb connObject = new ConnectionToDb();
-//    Connection connection = connObject.getConnection();
-//
-//    ChainDefinitionTest() throws SQLException {
-//    }
-//
-//
-//    @Test
-//    public void getSpecificChain() throws SQLException {
-//
-//        //prepare the db
+package tableDefinition;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChainDefinitionTest {
+    ChainDefinition emptyDefinition = new ChainDefinition();
+    ConnectionToDb connObject = new ConnectionToDb();
+    Connection connection = connObject.getConnection();
+    PrepareDbClass prepareDbClass = new PrepareDbClass();
+    @BeforeEach
+    public void prepareDb() throws SQLException {
+        prepareDbClass.prepareDb(connection);
+    }
+
+    ChainDefinitionTest() throws SQLException {
+    }
+
+
+    @Test
+    public void getSpecificChain() throws SQLException {
+
+        //prepare the db
 //        emptyDefinition.deleteSpecificChain(222);
-//        ChainDefinition definition = new ChainDefinition(222,"Fox kids");
-//        definition.createChain(connection);
-//        //the test
-//        int actual = emptyDefinition.getSpecificChain(222);
-//        //after test db cleaning
-//        emptyDefinition.deleteSpecificChain(222);
-//        assertEquals(222, actual );
-//    }
-//    @Test
-//    public void insertChain() throws SQLException {
-//     //prepare the db
-//          emptyDefinition.deleteSpecificChain(21);
-//        ChainDefinition definition = new ChainDefinition(21,"Fox kids");
-//        //the test
-//        int actual = definition.createChain(connection);
-//        //after test db cleaning
-//        definition.deleteSpecificChain(21);
-//        assertEquals(1, actual);
-//    }
-//    @Test
-//    public void deleteSpecificChain() throws SQLException {
-//        //prepare the db
-//        ChainDefinition definition = new ChainDefinition(22,"Fox kids");
-//        definition.createChain(connection);
-//        //delete specific chain
-//        int actual = emptyDefinition.deleteSpecificChain(22);
-//        assertEquals(1, actual);
-//    }
-//    }
-//
+        ChainDefinition definition = new ChainDefinition();
+        //definition.createChain(connection);
+        //the test
+        int actual = emptyDefinition.getSpecificChain(connection, 31);
+        //after test db cleaning
+        //emptyDefinition.deleteSpecificChain(222);
+        assertEquals(31, actual );
+    }
+    @Test
+    public void insertChain() throws SQLException {
+     //prepare the db
+         // emptyDefinition.deleteSpecificChain(21);
+        ChainDefinition definition = new ChainDefinition(222,"Fox kids");
+        //the test
+        int actual = definition.createChain(connection);
+        //after test db cleaning
+        //definition.deleteSpecificChain(21);
+        assertEquals(1, actual);
+    }
+    @Test
+    public void deleteSpecificChain() throws SQLException {
+        //prepare the db
+        ChainDefinition definition = new ChainDefinition(222,"Fox Kids");
+        definition.createChain(connection);
+        //delete specific chain
+        int actual = emptyDefinition.deleteSpecificChain(connection,222);
+        assertEquals(1, actual);
+    }
+    }
+
--- src/test/java/tableDefinition/EmployeeDefinitionTest.java
+++ src/test/java/tableDefinition/EmployeeDefinitionTest.java
@@ -1,70 +1,62 @@
-//package tableDefinition;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.omg.CORBA.PUBLIC_MEMBER;
-//
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class EmployeeDefinitionTest {
-//
-//    @BeforeEach
-//    private void beforeEmployeeHandle() throws SQLException {
-//        ChainDefinition chainDefinition = new ChainDefinition(21,"Fox kids");
-//        chainDefinition.createChain();
-//        ShopDefinition definition = new ShopDefinition(2, 21, 111, 25, "Tel-Aviv");
-//        definition.deleteSpecificShop(2);
-//        definition.createShop();
-//    }
-//    @Test
-//    public void insertEmployee() throws SQLException {
-//
-//        EmployeeDefinition definitionEmployee = new EmployeeDefinition(10,2, 21);
-//        int actual = definitionEmployee.createEmployee();
-//        assertEquals(1, actual);
-//    }
-//
-//    @Test
-//    public void getSpecificId() throws SQLException {
-//        //prepare db
-//        EmployeeDefinition definitionEmployee = new EmployeeDefinition(10,2, 21);
-//        definitionEmployee.createEmployee();
-//        //the test
-//        int actual = definitionEmployee.getSpecificEmployee(10);
-//        assertEquals(10, actual);
-//    }
-//
-//    @Test
-//    public void getAllEmployeeOfCertainChain() throws SQLException {
-//        EmployeeDefinition definitionEmployeesInChain = new EmployeeDefinition();
-//        List<Integer> expected = new ArrayList<>();
-//        expected.add(10);
-//        expected.add(11);
-//        expected.add(12);
-//        //prepare db
-//        Main.createThreeEmployees();
-//        //the test
-//        List<Integer> actual = definitionEmployeesInChain.getAllEmployeeInChain(21);
-//        assertTrue(expected.equals(actual));
-//    }
-//
-//    @AfterEach
-//    private void afterEmployeeHandle() throws SQLException {
-//
-//        EmployeeDefinition definitionEmployee = new EmployeeDefinition();
-//        definitionEmployee.deleteSpecificEmployee(10);
-//        EmployeeDefinition definitionEmployee1 = new EmployeeDefinition();
-//        definitionEmployee1.deleteSpecificEmployee(11);
-//        EmployeeDefinition definitionEmployee2 = new EmployeeDefinition();
-//        definitionEmployee2.deleteSpecificEmployee(12);
-//        ShopDefinition shopDefinition = new ShopDefinition();
-//        shopDefinition.deleteSpecificShop(2);
-//        ChainDefinition chainDefinition = new ChainDefinition();
-//        chainDefinition.deleteSpecificChain(21);
-//    }
-//}
+package tableDefinition;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeDefinitionTest {
+
+    ChainDefinition emptyDefinition = new ChainDefinition();
+    ConnectionToDb connObject = new ConnectionToDb();
+    Connection connection = connObject.getConnection();
+    PrepareDbClass prepareDbClass = new PrepareDbClass();
+
+    EmployeeDefinitionTest() throws SQLException {
+    }
+
+    @BeforeEach
+    public void prepareDb() throws SQLException {
+        prepareDbClass.prepareDb(connection);
+    }
+
+    @Test
+    public void insertEmployee() throws SQLException {
+
+        EmployeeDefinition definitionEmployee = new EmployeeDefinition(10,79, 21);
+        int actual = definitionEmployee.createEmployee(connection);
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void getSpecificEmployee() throws SQLException {
+
+        EmployeeDefinition definitionEmployee = new EmployeeDefinition(1976,89, 31);
+        definitionEmployee.createEmployee(connection);
+        int actual = definitionEmployee.getSpecificEmployee(connection, 1976);
+        assertEquals(1976, actual);
+    }
+
+    @Test
+    public void getAllEmployeeOfCertainChain() throws SQLException {
+        EmployeeDefinition definitionEmployeesInChain = new EmployeeDefinition();
+        List<Integer> expected = new ArrayList<>();
+        expected.add(1977);
+        List<Integer> actual = definitionEmployeesInChain.getAllEmployeeInChain(connection, 21);
+        assertTrue(expected.equals(actual));
+    }
+    @Test
+    public void deleteEmployee() throws SQLException {
+        EmployeeDefinition definitionEmployee = new EmployeeDefinition();
+        int actual = definitionEmployee.deleteSpecificEmployee(connection, 1977);
+        assertEquals(1, actual);
+    }
+
+}
